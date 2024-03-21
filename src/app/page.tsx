9178f1202d1db56434ef4c6d6bf3bd2d3695bfe8
Rohiteswar velagapudi <rohiteswar.velagapudi@gmail.com>
2024-03-21 17:41:36 +0530

    Font Loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,16 @@
-
 import { Journey } from "./Components/Customerjourney";
 import Hero from "./Components/Hero";
 
 export default function Home() {
   return (
     <>
-
-        <Hero />
-        <div className="mx-auto text-center text-6xl p-6 ">
-          Why <span>WOW</span> <span >Health</span>{" "}
-          <span className="text-[#EB2227]"> ?</span>
-        </div>
-        <Journey />
-        {/* <TypeWrite /> */}
-
-    
+      <Hero />
+      <div className="mx-auto text-center text-6xl p-6 text-black">
+        Why <span>WOW</span> <span>Health</span>{" "}
+        <span className="text-[#EB2227]"> ?</span>
+      </div>
+      <Journey />
+      {/* <TypeWrite /> */}
     </>
   );
 }
