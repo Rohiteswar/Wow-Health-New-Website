e3518b07b126f6612b1c5b6500723264992aaa0a
Rohiteswar velagapudi <rohiteswar.velagapudi@gmail.com>
2024-03-21 18:59:04 +0530

    Nav

---
+++ src/app/Contact-us/page.tsx
@@ -0,0 +1,12 @@
+import BasicForm from "../Components/Contact-us";
+
+
+export default function Contact_us() {
+  return (
+    <>
+      <div>
+        <BasicForm />
+      </div>
+    </>
+  );
+}
