d94c5c885bc07a8f34d1524d1305da5179ecd12e
rohiteswar <rohiteswar.velagapudi@gmail.com>
2024-04-26 07:13:06 +0530

    username

--- src/app/Components/ui/Deletion_form.tsx
+++ src/app/Components/ui/Deletion_form.tsx
@@ -59,7 +59,7 @@ export default function DeleteForm() {
                     className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-black"
                     htmlFor="full-name"
                   >
-                    Full Name
+                    Username
                   </label>
                   <input
                     className="text-slate-500 flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
@@ -67,7 +67,7 @@ export default function DeleteForm() {
                     value={name}
                     onChange={(e) => setName(e.target.value)}
                     id="full-name"
-                    placeholder="Enter your full name"
+                    placeholder="Enter your username"
                     name="Fullname"
                   />
                 </div>
